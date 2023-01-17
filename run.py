3a3c1566b809981b5265505a37ed10832c2242b7
RavoPL <ravosilesia88@protonmail.com>
2023-01-17 17:20:43 +0000

    Add main to run.py and disable cursor blinking

--- run.py
+++ run.py
@@ -4,3 +4,11 @@ which can be used to create a rectangular bounding box within the terminal
 """
 import curses
 from curses import textpad
+
+"""
+Main function that sets the bounding box border
+as well as the details of the Snake and keyboard input
+"""
+def main(stdscr):
+    # disables the blinking of the cursor in terminal
+    curses.curs_set(0)
